eaa3ffc14e2697856c4063e125b8ee7894ce0093
vimes <vimes@yandex-team.ru>
2024-04-22 21:12:25 +0300

    Control plane workers config parsing
    Adds parsing of control plane workers configuration from config file.
    Expected config file section is:
    
    "controlPlaneWorkers": [
        {
            "core" : 42,
            "interfaces": [ <interface_name_string> ]
        }
    ]
    
    In case section is not present, configuration for single control plane
    worker, responsible for all dataplane interfaces and residing on
    controlPlaneCoreId is generated. This Is done to ease transition on
    already deployed appliances.
    
    Section is checked for
    - Every configured dataplane interface is assigned to a control plane worker
    - Each interface is assigned to single control plane worker
    - Interfaces not configured for use by dataplane, but listed in
      controlplane workers are treated as errors

--- dataplane/dataplane.h
+++ dataplane/dataplane.h
@@ -39,7 +39,7 @@ struct tDataPlaneConfig
 	   and an identifier (typically pci id) used to lookup the port within
 	   DPDK.
 	*/
-	std::map<InterfaceName, ///< interfaceName
+	std::map<InterfaceName,
 	         std::tuple<std::string, ///< pci
 	                    std::string, ///< name
 	                    bool, ///< symmetric_mode
@@ -49,7 +49,7 @@ struct tDataPlaneConfig
 
 	std::set<tCoreId> workerGCs;
 	tCoreId controlPlaneCoreId;
-	std::map<tCoreId, std::vector<InterfaceName>> controlplane_workers;
+	std::map<tCoreId, std::set<InterfaceName>> controlplane_workers;
 	std::map<tCoreId, std::vector<InterfaceName>> workers;
 	bool useHugeMem = true;
 	bool use_kernel_interface = true;
@@ -114,10 +114,16 @@ public:
 protected:
 	eResult parseConfig(const std::string& configFilePath);
 	eResult parseJsonPorts(const nlohmann::json& json);
+	std::optional<std::map<tCoreId, std::set<InterfaceName>>> parseControlPlaneWorkers(const nlohmann::json& config);
+	std::optional<std::pair<tCoreId, std::set<InterfaceName>>> parseControlPlaneWorker(const nlohmann::json& cpwj);
+	nlohmann::json makeLegacyControlPlaneWorkerConfig();
+	std::set<InterfaceName> workerInterfacesToService();
 	eResult parseConfigValues(const nlohmann::json& json);
 	eResult parseRateLimits(const nlohmann::json& json);
 	eResult parseSharedMemory(const nlohmann::json& json);
 	eResult checkConfig();
+	bool checkControlPlaneWorkersConfig();
+	bool checkValidCoreId(const nlohmann::json& j);
 
 	eResult initEal(const std::string& binaryPath, const std::string& filePrefix);
 	eResult initPorts();
@@ -160,7 +166,7 @@ protected:
 	std::map<tPortId, KniHandleBundle> kni_interface_handles;
 
 	std::map<tPortId,
-	         std::tuple<std::string, ///< interface_name
+	         std::tuple<InterfaceName,
 	                    std::map<tCoreId, tQueueId>, ///< rx_queues
 	                    unsigned int, ///< tx_queues_count
 	                    common::mac_address_t, ///< mac_address
